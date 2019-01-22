afdaa662042bfc70fb1217fad0b32930e20ea05d
Bram <shep.ukn@gmail.com>
2019-01-22 01:52:58 +0100

    Updated the routes, indexes and package.json to fit authentication

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,20 @@
 const ProductsController = require ('../controllers/product_controller')
 
 module.exports = (app) => {
+  // USERS.
+  app.get('/api/users/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+    res.json({user: req.user});
+  });
+
+  app.post('/api/users/register',  UserController.register);
+  app.post('/api/users/authenticate',  UserController.authenticate);
+
+  app.get('/api/users',  UserController.read);
+  app.get('/api/users/:id',  UserController.readById);
+  // app.put('/api/users/:id',  UserController.edit);
+  // app.delete('/api/users/:id',  UserController.delete);
+
+  // PRODUCTS.
   app.get('/api', ProductsController.greeting);
   app.get('/api/products/:id',  ProductsController.readById);
   app.get('/api/products', ProductsController.read);
